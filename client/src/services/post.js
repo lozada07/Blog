c4bb3a9d41862da9fa301725a5ade405b1b8b7e7
lozada07 <135869909+lozada07@users.noreply.github.com>
2023-12-23 20:02:27 -0400

    Update post.js

--- client/src/services/post.js
+++ client/src/services/post.js
@@ -1,7 +1,9 @@
 import { BACKEND_URL, getToken } from "../config";
 
 export const getAllPosts = async (query) => {
-  const res = await fetch(`${BACKEND_URL}/api/v1/posts?q=${query}`, {
+  const q = query === "null" ? "" : query
+  
+  const res = await fetch(`${BACKEND_URL}/api/v1/posts?q=${q}`, {
     mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json'
