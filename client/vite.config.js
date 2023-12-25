78c87fb8af4f7c62d5e4ebda3cc8d19b7427ce59
lozada07 <135869909+lozada07@users.noreply.github.com>
2023-12-25 19:49:07 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-},
-build: {
+  build: {
     chunkSizeWarningLimit: 1600,
   },
-)
+})
